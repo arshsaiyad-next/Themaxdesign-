35383f58868f9cd88257e48d288d657c98f22d97
arsh saiyad <arshsaiyad850@gmail.com>
2025-10-26 23:08:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// // // document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scrolling for navbar links
+                    const navLinks = document.querySelectorAll('.nav-links a');
+                                                            navLinks.forEach(link => {
+                                                                                                                                                link.addEventListener('click', (e) => {                       const observer = new IntersectionObserver((entries) =>                       const observer = new IntersectionObserver((entries) =>                       const observer = new IntersectionObserver((entries) =>   const observer = new IntersectionObserver((entries) =>   const observer = new IntersectionObserver((entries) =>
